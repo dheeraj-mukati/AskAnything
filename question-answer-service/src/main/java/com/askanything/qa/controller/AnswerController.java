2549ebcc3ab76035e60db0ce0d6db9fdca9bc62b
Dheeraj Mukati <dheeraj.mukati@yahoo.com>
2018-12-30 20:58:42 -0600

    Added CrossOrigin annotation on all the RestControllers

--- question-answer-service/src/main/java/com/askanything/qa/controller/AnswerController.java
+++ question-answer-service/src/main/java/com/askanything/qa/controller/AnswerController.java
@@ -3,6 +3,7 @@ package com.askanything.qa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.askanything.qa.entity.Question;
 import com.askanything.qa.repository.AnswerRepository;
 
 @RestController
+@CrossOrigin
 public class AnswerController {
 
 	@Autowired
--- question-answer-service/src/main/java/com/askanything/qa/controller/CategoryController.java
+++ question-answer-service/src/main/java/com/askanything/qa/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.askanything.qa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.askanything.qa.entity.Category;
 import com.askanything.qa.repository.CategoryRepository;
 
 @RestController
+@CrossOrigin
 public class CategoryController {
 
 	@Autowired
