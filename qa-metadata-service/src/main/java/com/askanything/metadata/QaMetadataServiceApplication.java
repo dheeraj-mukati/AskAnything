1518549bef1ba6a8e866eeafc1b74b5dedea9472
Dheeraj Mukati <dheeraj.mukati@yahoo.com>
2019-01-21 11:37:37 -0600

    New microservice to get metadata of Questions and Answers

---
+++ qa-metadata-service/src/main/java/com/askanything/metadata/QaMetadataServiceApplication.java
@@ -0,0 +1,14 @@
+package com.askanything.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaMetadataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaMetadataServiceApplication.class, args);
+	}
+
+}
+
